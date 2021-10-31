5d30b02ca9e87a9ef73f4ac5b547665742a6edf7
kabachan <>
2021-10-31 15:15:43 +0900

    余分なview、pathの消去

--- blogSample/Dblog/urls.py
+++ blogSample/Dblog/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('home/', views.home, name='home'),
-    path('register/', views.register, name='register'),
+  
+   
    
 ]
