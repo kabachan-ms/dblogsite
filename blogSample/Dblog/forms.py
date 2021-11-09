67940de70cd58e9b52fc5fa5d0cca9e0ace0c555
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2021-11-10 08:08:32 +0900

    デグレを修正

--- blogSample/Dblog/forms.py
+++ blogSample/Dblog/forms.py
@@ -6,6 +6,6 @@ from django import forms
 from django.contrib.auth.models import User
 
 class PostForm(forms.Form):
-    content = MDTextFormField() # 変更
     title = forms.CharField(max_length=100,label="タイトル")
+    content = MDTextFormField() # 変更
     username = models.ForeignKey(User, to_field = 'username',on_delete=models.CASCADE)
--- blogSample/Dblog/views.py
+++ blogSample/Dblog/views.py
@@ -2,20 +2,47 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
-
+from .models import Article
+import datetime
 
 
 @login_required
+# def post_form(request):
+#     if request.method == "POST":
+#         postForm = PostForm(request.POST)
+#         if postForm.is_valid():
+#             postForm = postForm.save(commit=False)
+#             # reauestにユーザ情報を含められるようにする必要がある
+#             # postForm.author = request.username
+#             # postForm.published_date = timezone.now()
+#             # postForm.save()
+#             # 記事を作成したらマイページトップに遷移するようにする
+#     else:
+#         postForm = PostForm()
+#     return render(request, 'Dblog/makeBlog.html', {'postForm': postForm})
+
 def post_form(request):
+    user = request.user
+    postForm = PostForm(request.POST)
+    if user.is_active:
+        print(user.username)
+    else:
+        print("can't get user information.")
+
     if request.method == "POST":
         postForm = PostForm(request.POST)
         if postForm.is_valid():
-            postForm = postForm.save(commit=False)
-            # reauestにユーザ情報を含められるようにする必要がある
-            # postForm.author = request.username
-            # postForm.published_date = timezone.now()
-            # postForm.save()
-            # 記事を作成したらマイページトップに遷移するようにする
+            title = postForm.cleaned_data['title']
+            content = postForm.cleaned_data['content']
+            username = user
+            Article.objects.create(
+                title=title,
+                content=content,
+                username=username,
+                created=datetime.datetime.now(),
+                is_delete=0,
+                is_private=0,
+            )
     else:
         postForm = PostForm()
     return render(request, 'Dblog/makeBlog.html', {'postForm': postForm})
